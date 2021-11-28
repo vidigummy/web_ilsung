298cee36911f8ef7a1314e3b88ac2bcd512b8c2b
vidigummy <vidigummy@gmail.com>
2021-11-28 22:02:32 +0900

    dd

--- api/db_connect.php
+++ api/db_connect.php
@@ -1,34 +1,33 @@
 <?php
     // echo("ddd");
     $conn = mysqli_connect('localhost', 'ryu', 'fbehddls147!', "ilsung");
-    if (mysqli_connect_errno($conn))
-{
-  echo "데이터베이스 연결 실패: " . mysqli_connect_error();
-}
-else
-{
-  echo "데이터베이스 연결 성공\n";
-// to do something
-
-
-}
-
-function sql_query($sql){
-	
-    global $conn;
-	//lee 201007 오류수정 조건 stripos($sql,"select") != false) 를 stripos($sql,"select") !== false) 로 수정.
-	$is_select = (stripos($sql,"select") < 3 && stripos($sql,"select") !== false) ? 1 : 0;
-	//var_dump(stripos($sql,"select"));
-	//var_dump(stripos($sql,"select") != false);
-	if($is_select){
-		//return "select문입니다.";
-		//echo "select문입니다.";
-		return $conn->query($sql);
-	}else{
-		//echo "select문이 아닙니다";
-    	return $conn->query($sql);
+    if (mysqli_connect_errno($conn)){
+  		echo "데이터베이스 연결 실패: " . mysqli_connect_error();
 	}
-	//error_log("QUERY  DB : $LAST_CALLED_DB");
-}
- 
+	else{
+  		echo "데이터베이스 연결 성공\n";
+		// to do something
+
+
+	}
+
+	function sql_query($sql){
+		
+		global $conn;
+		//lee 201007 오류수정 조건 stripos($sql,"select") != false) 를 stripos($sql,"select") !== false) 로 수정.
+		$is_select = (stripos($sql,"select") < 3 && stripos($sql,"select") !== false) ? 1 : 0;
+		//var_dump(stripos($sql,"select"));
+		//var_dump(stripos($sql,"select") != false);
+		if($is_select){
+			//return "select문입니다.";
+			//echo "select문입니다.";
+			// echo("ssibal");
+			return $conn->query($sql);
+		}else{
+			//echo "select문이 아닙니다";
+			return $conn->query($sql);
+		}
+		//error_log("QUERY  DB : $LAST_CALLED_DB");
+	}
+	
 ?>
--- api/sign_in_check.php
+++ api/sign_in_check.php
@@ -5,27 +5,36 @@
     $passwd_hashed = hash("sha256", $password);
     // $arr = sql_query("SELECT * FROM USER");
     
-    $res = sql_query("SELECT idx, user_level, usr_passwd FROM USER WHERE urs_id LIKE '{$user_id}'");
-    echo("SELECT idx, user_level, usr_passwd FROM USER WHERE usr_id LIKE '{$user_id}'");
+    $res = sql_query("SELECT * FROM USER WHERE urs_id LIKE '{$user_id}'");
+    echo("SELECT * FROM USER WHERE usr_id LIKE '{$user_id}'");
     
-    while($row = $res -> fetch_assoc()){
-        $passwd_db = $row['usr_passwd'];
-        $usr_level = $row['user_level'];
-        $usr_passwd = $row['usr_passwd'];
-        
-        echo("yes");
-        if($passwd_db == $passwd_hashed){
-            
-            $result = array();
-            array_push($result, array('try' => "success", 'usr_idx' => $usr_level, 'usr_level' => $usr_passwd));
-            echo json_encode(array("result" => $result), JSON_UNESCAPED_UNICODE);
-        }
-        else{
-            $result = array();
-            array_push($result, array('try' => "fail", 'usr_idx' => '-1', 'usr_level' => '-1'));
-            echo json_encode(array("result" => $result), JSON_UNESCAPED_UNICODE);
-        }
-    }
+    $row = mysqli_fetch_assoc($res);
+    echo($row);
+    print_r($row);
+    // while($row = mysqli_fetch_array($res)){
+    //     $idx = $row[0];
+    //     echo($idx);
+    // }
+    
+
+    // $row = mysqli_fetch_assoc($res);
+    // $passwd_db = $row[0]['usr_passwd'];
+    // $usr_level = $row[0]['user_level'];
+    // $usr_idx = $row[0]['idx'];
+    // echo($passwd_hashed."\n");
+    // echo("???");
+    // echo($usr_idx);
+    // if($passwd_db == $passwd_hashed){
+    //     echo("yes");
+    //     $result = array();
+    //     array_push($result, array('try' => "success", 'usr_idx' => $usr_idx, 'usr_level' => $usr_level));
+    //     echo json_encode(array("result" => $result), JSON_UNESCAPED_UNICODE);
+    // }
+    // else{
+    //     $result = array();
+    //     array_push($result, array('try' => "fail", 'usr_idx' => '-1', 'usr_level' => '-1'));
+    //     echo json_encode(array("result" => $result), JSON_UNESCAPED_UNICODE);
+    // }
 
 
     // $arr = sql_query("SELECT * FROM USER WHERE usr_id LIKE '".$user_id."'");
